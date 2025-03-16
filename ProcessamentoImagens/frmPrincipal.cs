0d09f446a7b9e061f0f2e69f6153d0849c81336a
byelBorges <gabryelhenborges@gmail.com>
2025-03-16 17:06:33 -0300

    feat: algoritmo de retas implementados

--- ProcessamentoImagens/frmPrincipal.cs
+++ ProcessamentoImagens/frmPrincipal.cs
@@ -1,65 +1,217 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using System.IO;
-using System.Drawing.Imaging;
 
 namespace ProcessamentoImagens
 {
     public partial class frmPrincipal : Form
     {
-        private Image image;
-        private Bitmap imageBitmap;
-
+        private Bitmap canvas;
         private bool flag = true;
         private int x1, y1, x2, y2;
+        private Point shadowEnd;
 
         public frmPrincipal()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized; // Maximiza a janela ao iniciar
+            this.WindowState = FormWindowState.Maximized;
             comboBoxAlgoritmos.SelectedIndex = 1;
+
+            // Habilitar Double Buffering usando reflexão
+            panel1.GetType().InvokeMember(
+                "DoubleBuffered",
+                System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic,
+                null,
+                panel1,
+                new object[] { true }
+            );
+
+            // Inicializar canvas com o tamanho atual do panel1
+            UpdateCanvasSize();
+            ClearCanvas();
+
+            // Associar o evento de redimensionamento
+            this.Resize += frmPrincipal_Resize;
         }
 
-        private void btnAbrirImg_Click(object sender, EventArgs e)
+        private void UpdateCanvasSize()
         {
-            openFileDialog.FileName = "";
-            openFileDialog.Filter = "Arquivos de Imagem (*.jpg;*.gif;*.bmp;*.png)|*.jpg;*.gif;*.bmp;*.png";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            if (canvas != null) canvas.Dispose(); // Liberar o recurso anterior
+            canvas = new Bitmap(panel1.Width, panel1.Height);
+        }
+
+        private void ClearCanvas()
+        {
+            using (Graphics g = Graphics.FromImage(canvas))
             {
-                image = Image.FromFile(openFileDialog.FileName);
-                pictBoxImg.Image = image;
-                pictBoxImg.SizeMode = PictureBoxSizeMode.StretchImage; // Ajusta a imagem para preencher o PictureBox
+                g.Clear(Color.White);
             }
+            panel1.Invalidate();
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
-            pictBoxImg.Image = null;
+            ClearCanvas();
+            flag = true;
         }
 
-        private void pictBoxImg_Click(object sender, EventArgs e)
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
-            MouseEventArgs mouseEv = (MouseEventArgs)e;
-            int mouseX = mouseEv.X;
-            int mouseY = mouseEv.Y;
-
-            if (flag)//se primeiro click
+            if (!flag)
             {
-                x1 = mouseX;
-                y1 = mouseY;
+                shadowEnd = new Point(e.X, e.Y);
+                panel1.Invalidate();
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (flag)
+            {
+                x1 = e.X;
+                y1 = e.Y;
                 Console.WriteLine("Primeiro click: Coord -> X = " + x1 + " Y = " + y1);
             }
             else
             {
-                x2 = mouseX;
-                y2 = mouseY;
+                x2 = e.X;
+                y2 = e.Y;
                 Console.WriteLine("Segundo click: Coord -> X = " + x2 + " Y = " + y2);
+
+                using (Graphics g = Graphics.FromImage(canvas))
+                {
+                    switch (comboBoxAlgoritmos.SelectedIndex)
+                    {
+                        case 0: DrawLineDDA(g, canvas, x1, y1, x2, y2, new Pen(Color.Red)); break;
+                        case 1: DrawLineGeneralEquation(g, canvas, x1, y1, x2, y2, new Pen(Color.Blue)); break;
+                        case 2: DrawLineMidpoint(g, canvas, x1, y1, x2, y2, new Pen(Color.Green)); break;
+                    }
+                }
+                panel1.Invalidate();
             }
             flag = !flag;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(canvas, 0, 0);
+            if (!flag && shadowEnd != Point.Empty)
+            {
+                using (Pen shadowPen = new Pen(Color.Gray, 1))
+                {
+                    switch (comboBoxAlgoritmos.SelectedIndex)
+                    {
+                        case 0: DrawLineDDA(e.Graphics, canvas, x1, y1, shadowEnd.X, shadowEnd.Y, shadowPen); break;
+                        case 1: DrawLineGeneralEquation(e.Graphics, canvas, x1, y1, shadowEnd.X, shadowEnd.Y, shadowPen); break;
+                        case 2: DrawLineMidpoint(e.Graphics, canvas, x1, y1, shadowEnd.X, shadowEnd.Y, shadowPen); break;
+                    }
+                }
+            }
+        }
+
+        private void frmPrincipal_Resize(object sender, EventArgs e)
+        {
+            UpdateCanvasSize();
+            ClearCanvas();
+        }
+
+        private void DrawLineDDA(Graphics g, Bitmap bmp, int x1, int y1, int x2, int y2, Pen pen)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+            float xIncrement = dx / (float)steps;
+            float yIncrement = dy / (float)steps;
+            float x = x1;
+            float y = y1;
+
+            for (int i = 0; i <= steps; i++)
+            {
+                if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+                    g.DrawRectangle(pen, x, y, 1, 1);
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        private void DrawLineGeneralEquation(Graphics g, Bitmap bmp, int x1, int y1, int x2, int y2, Pen pen)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            if (dx == 0)
+            {
+                int yStart = Math.Min(y1, y2);
+                int yEnd = Math.Max(y1, y2);
+                for (int y = yStart; y <= yEnd; y++)
+                    if (x1 >= 0 && x1 < bmp.Width && y >= 0 && y < bmp.Height)
+                        g.DrawRectangle(pen, x1, y, 1, 1);
+                return;
+            }
+
+            if (dy == 0)
+            {
+                int xStart = Math.Min(x1, x2);
+                int xEnd = Math.Max(x1, x2);
+                for (int x = xStart; x <= xEnd; x++)
+                    if (x >= 0 && x < bmp.Width && y1 >= 0 && y1 < bmp.Height)
+                        g.DrawRectangle(pen, x, y1, 1, 1);
+                return;
+            }
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                int xStart = Math.Min(x1, x2);
+                int xEnd = Math.Max(x1, x2);
+                for (int x = xStart; x <= xEnd; x++)
+                {
+                    int y = y1 + (dy * (x - x1)) / dx;
+                    if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+                        g.DrawRectangle(pen, x, y, 1, 1);
+                }
+            }
+            else
+            {
+                int yStart = Math.Min(y1, y2);
+                int yEnd = Math.Max(y1, y2);
+                for (int y = yStart; y <= yEnd; y++)
+                {
+                    int x = x1 + (dx * (y - y1)) / dy;
+                    if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+                        g.DrawRectangle(pen, x, y, 1, 1);
+                }
+            }
+        }
+
+        private void DrawLineMidpoint(Graphics g, Bitmap bmp, int x1, int y1, int x2, int y2, Pen pen)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int sx = x1 < x2 ? 1 : -1;
+            int sy = y1 < y2 ? 1 : -1;
+            int err = dx - dy;
+            int x = x1;
+            int y = y1;
+
+            while (true)
+            {
+                if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+                    g.DrawRectangle(pen, x, y, 1, 1);
+
+                if (x == x2 && y == y2) break;
+
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y += sy;
+                }
+            }
         }
     }
 }
--- ProcessamentoImagens/frmPrincipal.designer.cs
+++ ProcessamentoImagens/frmPrincipal.designer.cs
@@ -17,29 +17,14 @@
 
         private void InitializeComponent()
         {
-            this.pictBoxImg = new System.Windows.Forms.PictureBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.panelOpcoes = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.comboBoxAlgoritmos = new System.Windows.Forms.ComboBox();
             this.buttonLimparImg = new System.Windows.Forms.Button();
-            this.buttonAbrirImg = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg)).BeginInit();
             this.panelOpcoes.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictBoxImg
-            // 
-            this.pictBoxImg.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictBoxImg.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictBoxImg.Location = new System.Drawing.Point(0, 0);
-            this.pictBoxImg.Name = "pictBoxImg";
-            this.pictBoxImg.Size = new System.Drawing.Size(725, 509);
-            this.pictBoxImg.TabIndex = 102;
-            this.pictBoxImg.TabStop = false;
-            this.pictBoxImg.Click += new System.EventHandler(this.pictBoxImg_Click);
             // 
             // panelOpcoes
             // 
@@ -48,12 +33,20 @@
             this.panelOpcoes.Controls.Add(this.label1);
             this.panelOpcoes.Controls.Add(this.comboBoxAlgoritmos);
             this.panelOpcoes.Controls.Add(this.buttonLimparImg);
-            this.panelOpcoes.Controls.Add(this.buttonAbrirImg);
             this.panelOpcoes.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelOpcoes.Location = new System.Drawing.Point(725, 0);
+            this.panelOpcoes.Location = new System.Drawing.Point(728, 0);
             this.panelOpcoes.Name = "panelOpcoes";
-            this.panelOpcoes.Size = new System.Drawing.Size(255, 509);
+            this.panelOpcoes.Size = new System.Drawing.Size(252, 509);
             this.panelOpcoes.TabIndex = 103;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Algoritmo:";
             // 
             // comboBoxAlgoritmos
             // 
@@ -70,7 +63,7 @@
             // buttonLimparImg
             // 
             this.buttonLimparImg.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonLimparImg.Location = new System.Drawing.Point(19, 32);
+            this.buttonLimparImg.Location = new System.Drawing.Point(18, 32);
             this.buttonLimparImg.Name = "buttonLimparImg";
             this.buttonLimparImg.Size = new System.Drawing.Size(215, 23);
             this.buttonLimparImg.TabIndex = 1;
@@ -78,35 +71,17 @@
             this.buttonLimparImg.UseVisualStyleBackColor = true;
             this.buttonLimparImg.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
-            // buttonAbrirImg
-            // 
-            this.buttonAbrirImg.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonAbrirImg.Location = new System.Drawing.Point(19, 3);
-            this.buttonAbrirImg.Name = "buttonAbrirImg";
-            this.buttonAbrirImg.Size = new System.Drawing.Size(215, 23);
-            this.buttonAbrirImg.TabIndex = 0;
-            this.buttonAbrirImg.Text = "Abrir imagem";
-            this.buttonAbrirImg.UseVisualStyleBackColor = true;
-            this.buttonAbrirImg.Click += new System.EventHandler(this.btnAbrirImg_Click);
-            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel1.Controls.Add(this.pictBoxImg);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(725, 509);
+            this.panel1.Size = new System.Drawing.Size(728, 509);
             this.panel1.TabIndex = 104;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Algoritmo:";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             // 
             // frmPrincipal
             // 
@@ -118,20 +93,15 @@
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Formulário Principal";
-            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg)).EndInit();
             this.panelOpcoes.ResumeLayout(false);
             this.panelOpcoes.PerformLayout();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictBoxImg;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Panel panelOpcoes;
-        private System.Windows.Forms.Button buttonAbrirImg;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button buttonLimparImg;
         private System.Windows.Forms.ComboBox comboBoxAlgoritmos;
